1c3103e908714834b3af40e7d97d48489291de2b
pwnely <daniel_040@live.nl>
2024-06-18 08:24:02 -0400

    todo

---
+++ travel_recommendation.js
@@ -0,0 +1,18 @@
+// task 6
+// function -> getData() => use fetch api to get data from travel_recommendation_api.json
+
+// task 7
+// function -> searchByKeyword() => link to button in html
+// get value from search input (html)
+// convert to lowercase (use function)
+// filter getData result based on keyword -> temples and countries.
+// display results in webpage (div id="name")
+
+// task 8
+// recommedations -> getRecommedations() =>
+// take keyword, find recommedations for keywords
+// display results in html
+
+
+// task 9
+// clearSearch() => remove values from input feilds + reset html
